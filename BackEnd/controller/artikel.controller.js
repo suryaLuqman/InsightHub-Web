04fde3f69769187420f8d6d66f16e053bf4c1d8e
jenien <riska.jeen@gmail.com>
2024-03-26 00:37:07 +0700

    feat: create artikel by user

--- BackEnd/controller/artikel.controller.js
+++ BackEnd/controller/artikel.controller.js
@@ -1,27 +1,35 @@
 require('dotenv').config();
+const { imagekit } = require('../libs/imagekit');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const prisma = require('../libs/prisma');
 
-// Create Artikel
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(' ')[1];
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = decoded;
+    next();
+  });
+};
+
 const createArtikel = async (req, res, next) => {
   try {
-    const { judul, deskripsi, link, gambar_artikel, kategoriId, authorId } = req.body;
-
-    let missingFields = [];
-
-    if (!judul) missingFields.push("judul");
-    if (!deskripsi) missingFields.push("deskripsi");
-    if (!link) missingFields.push("link");
-    if (!kategoriId) missingFields.push("kategoriId");
-    if (!authorId) missingFields.push("authorId");
-
-    // Jika ada field yang hilang, kembalikan error dengan detail field yang hilang.
-    if (missingFields.length > 0) {
-      return res.status(400).json({
-        success: false,
-        message: `Missing required fields: ${missingFields.join(", ")}`,
-        data: missingFields,
-      });
-    }
+    const { judul, deskripsi, link, kategoriId } = req.body;
+    const strFile = req.file.buffer.toString('base64');
+    
+    const hash = crypto.createHash('sha256').update(strFile).digest('hex');
+    
+    const { url} = await imagekit.upload({
+      fileName: Date.now() + path.extname(req.file.originalname),
+      file: strFile,
+    });
 
     const existingArtikel = await prisma.artikel.findFirst({
       where: {
@@ -29,67 +37,73 @@ const createArtikel = async (req, res, next) => {
       },
     });
 
-    // Periksa apakah gambar_artikel telah digunakan sebelumnya
-    const existingArtikelWithSameImage = await prisma.artikel.findFirst({
-      where: {
-        gambar_artikel,
-      },
-    });
-    
-
     if (existingArtikel) {
       return res.status(400).json({
-        success: false,
-        message: 'Judul artikel sudah ada, harap gunakan judul yang berbeda',
+        status: false,
+        message: 'Judul artikel sudah pernah digunakan, mohon gunakan judul lain',
         data: null,
       });
     }
 
-    // Jika artikel dengan gambar yang sama ditemukan, kirim respons dengan pesan kesalahan
-    if (existingArtikelWithSameImage) {
+    // Cek apakah hash dari gambar sudah ada dalam database
+    const existingHash = await prisma.artikel.findFirst({
+      where: {
+        fileId: hash,
+      },
+    });
+
+    if (existingHash) {
       return res.status(400).json({
-        success: false,
-        message: 'Gambar sudah digunakan oleh artikel lain. Harap gunakan gambar orisinal.',
+        status: false,
+        message: 'Gambar sudah pernah diunggah sebelumnya',
         data: null,
       });
     }
-
+    // Cek apakah kategori dengan kategoriId yang diberikan ada dalam database
     const existingKategori = await prisma.kategori.findUnique({
       where: {
-        id: kategoriId,
+        id: parseInt(kategoriId),
       },
     });
 
     if (!existingKategori) {
+      return res.status(404).json({
+        status: false,
+        message: 'Kategori not found',
+        data: null,
+      });
+    }
+    const artikel = await prisma.artikel.create({
+      data: {
+        author: { connect: { id: req.user.id } },
+        judul,
+        deskripsi,
+        link,
+        kategori: { connect: { id:parseInt( kategoriId) } },
+        gambar_artikel: url,
+        fileId:hash
+      }
+    });
+
+    if (!artikel) {
       return res.status(400).json({
-        success: false,
-        message: 'Invalid kategoriId',
+        status: false,
+        message: 'Bad Request!',
+        err: 'Failed to create article',
         data: null,
       });
     }
 
-    const newArtikel = await prisma.artikel.create({
-      data: {
-        judul,
-        deskripsi,
-        link,
-        gambar_artikel,
-        kategoriId,
-        authorId,
-      },
+    return res.status(200).json({
+      status: true,
+      message: 'OK!',
+      err: null,
+      data: { artikel },
     });
-
-    res.status(201).json({
-      success: true,
-      message: 'Artikel created successfully',
-      data: newArtikel,
-    });
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 };
-
-
 
 
 // Read Artikel
@@ -108,89 +122,89 @@ const getAllArtikel = async (req, res, next) => {
 };
 
 // Update Artikel
-const updateArtikel = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { judul, deskripsi, link, gambar_artikel, kategoriId, authorId } = req.body;
+// const updateArtikel = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const { judul, deskripsi, link, gambar_artikel, kategoriId, authorId } = req.body;
 
-    const existingArtikel = await prisma.artikel.findUnique({
-      where: {
-        id: parseInt(id),
-      },
-    });
+//     const existingArtikel = await prisma.artikel.findUnique({
+//       where: {
+//         id: parseInt(id),
+//       },
+//     });
 
-    if (!existingArtikel) {
-      return res.status(404).json({
-        success: false,
-        message: 'Artikel tidak ditemukan',
-        data: null,
-      });
-    }
+//     if (!existingArtikel) {
+//       return res.status(404).json({
+//         success: false,
+//         message: 'Artikel tidak ditemukan',
+//         data: null,
+//       });
+//     }
 
-    // Periksa judul jika akan diubah dan apakah judul tersebut sudah digunakan oleh artikel lain
-    if (judul !== existingArtikel.judul) {
-      const artikelWithSameJudul = await prisma.artikel.findFirst({
-        where: {
-          judul: judul,
-          NOT: {
-            id: parseInt(id),
-          },
-        },
-      });
+//     // Periksa judul jika akan diubah dan apakah judul tersebut sudah digunakan oleh artikel lain
+//     if (judul !== existingArtikel.judul) {
+//       const artikelWithSameJudul = await prisma.artikel.findFirst({
+//         where: {
+//           judul: judul,
+//           NOT: {
+//             id: parseInt(id),
+//           },
+//         },
+//       });
 
-      if (artikelWithSameJudul) {
-        return res.status(400).json({
-          success: false,
-          message: 'Judul artikel sudah ada, harap gunakan judul yang berbeda',
-          data: null,
-        });
-      }
-    }
+//       if (artikelWithSameJudul) {
+//         return res.status(400).json({
+//           success: false,
+//           message: 'Judul artikel sudah ada, harap gunakan judul yang berbeda',
+//           data: null,
+//         });
+//       }
+//     }
 
-    // Periksa gambar jika akan diubah dan apakah gambar tersebut sudah digunakan oleh artikel lain
-    if (gambar_artikel !== existingArtikel.gambar_artikel) {
-      const artikelWithSameImage = await prisma.artikel.findFirst({
-        where: {
-          gambar_artikel: gambar_artikel,
-          NOT: {
-            id: parseInt(id),
-          },
-        },
-      });
+//     // Periksa gambar jika akan diubah dan apakah gambar tersebut sudah digunakan oleh artikel lain
+//     if (gambar_artikel !== existingArtikel.gambar_artikel) {
+//       const artikelWithSameImage = await prisma.artikel.findFirst({
+//         where: {
+//           gambar_artikel: gambar_artikel,
+//           NOT: {
+//             id: parseInt(id),
+//           },
+//         },
+//       });
 
-      if (artikelWithSameImage) {
-        return res.status(400).json({
-          success: false,
-          message: 'Gambar sudah digunakan oleh artikel lain. Harap gunakan gambar orisinal.',
-          data: null,
-        });
-      }
-    }
+//       if (artikelWithSameImage) {
+//         return res.status(400).json({
+//           success: false,
+//           message: 'Gambar sudah digunakan oleh artikel lain. Harap gunakan gambar orisinal.',
+//           data: null,
+//         });
+//       }
+//     }
 
-    // Melanjutkan proses update jika tidak ada masalah
-    const updatedArtikel = await prisma.artikel.update({
-      where: {
-        id: parseInt(id),
-      },
-      data: {
-        judul,
-        deskripsi,
-        link,
-        gambar_artikel,
-        kategoriId,
-        authorId,
-      },
-    });
+//     // Melanjutkan proses update jika tidak ada masalah
+//     const updatedArtikel = await prisma.artikel.update({
+//       where: {
+//         id: parseInt(id),
+//       },
+//       data: {
+//         judul,
+//         deskripsi,
+//         link,
+//         gambar_artikel,
+//         kategoriId,
+//         authorId,
+//       },
+//     });
 
-    res.status(200).json({
-      success: true,
-      message: 'Artikel updated successfully',
-      data: updatedArtikel,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.status(200).json({
+//       success: true,
+//       message: 'Artikel updated successfully',
+//       data: updatedArtikel,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 
 // Delete Artikel
@@ -217,6 +231,7 @@ const deleteArtikel = async (req, res, next) => {
 module.exports = {
   createArtikel,
   getAllArtikel,
-  updateArtikel,
+  // updateArtikel,
   deleteArtikel,
+  authenticate
 };
--- BackEnd/routes/artikel.routes.js
+++ BackEnd/routes/artikel.routes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const artikelController = require('../controller/artikel.controller');
+const { createArtikel,getAllArtikel, updateArtikel,deleteArtikel, authenticate }=require('../controller/artikel.controller');
+const { image } = require ('../libs/multer');
+
 
 // Route untuk membuat artikel baru
-router.post('/add', artikelController.createArtikel);
+router.post('/upload', image.single('gambar_artikel'),authenticate,createArtikel );
 
 // Route untuk mendapatkan semua artikel
-router.get('/get-all', artikelController.getAllArtikel);
+router.get('/get-all', getAllArtikel);
 
 // Route untuk memperbarui artikel berdasarkan ID
-router.put('/edit/:id', artikelController.updateArtikel);
+// router.put('/updateArtikel/:id',authenticate, updateArtikel);
 
 // Route untuk menghapus artikel berdasarkan ID
-router.delete('/delete/:id', artikelController.deleteArtikel);
+router.delete('/delete/:id', deleteArtikel);
 
 module.exports = router;
