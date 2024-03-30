d9073d537c7189175ddf107ac7ad4eef2b8ff647
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-03-31 06:23:56 +0700

    test : get data api

--- Web-Insight-Hub/app.js
+++ Web-Insight-Hub/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const contohRoute = require('./routes/contohRoute');
+const path = require('path'); // Menambahkan baris ini untuk mengimpor modul path
 
-// Mengatur EJS sebagai view engine
+
+// Atur direktori views
+app.set('views', path.join(__dirname, 'app', 'views'));
+
+// Atur engine view yang digunakan (contoh menggunakan EJS)
 app.set('view engine', 'ejs');
 
 // Menggunakan middleware untuk mengatur rute
--- Web-Insight-Hub/app/controllers/contohController.js
+++ Web-Insight-Hub/app/controllers/contohController.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 
 exports.getContohPage = async (req, res) => {
     try {
-        const response = await axios.get('https://insight-hub-api.vercel.app/api/v1/artikel/get-all');
-        const articles = response.data;
-
-        res.render('contohView', { articles });
+        const response = await axios.get('https://insight-hub-api.vercel.app/api/v1/kategori/get-all');
+        console.log("response : ", response);
+        const dataKategori = response.data;
+         console.log('Data kategori:', dataKategori); // Tambahkan ini untuk mencetak data artikel
+        res.render('contohView', {articles: dataKategori });
     } catch (error) {
         console.error('Gagal mengambil data dari API:', error);
         res.status(500).send('Terjadi kesalahan');
