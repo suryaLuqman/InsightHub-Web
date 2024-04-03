9646640f551270ed6f6fc81e7f577008ef8a18ce
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-04-03 17:23:41 +0700

    fix : view artikel

--- BackEnd/views/routes/Artikel.View.Routes.js
+++ BackEnd/views/routes/Artikel.View.Routes.js
@@ -4,5 +4,6 @@ const artikelController = require('../controller/artikelController');
 
 // Define route for dashboard
 router.get('/:first_name/:id/add-artikel', artikelController.getArtikelPage);
+router.get('/:id/:judul', artikelController.getViewArtikelPage);
 
 module.exports = router;
